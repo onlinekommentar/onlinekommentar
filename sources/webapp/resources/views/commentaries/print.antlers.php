f71f9830d0c75f5db98108d5bab7576e42b0a132
Jack Sleight <jacksleight@gmail.com>
2026-09-09 20:32:20 +0100

    Add running titles and a processing status section to the print template

--- sources/webapp/resources/views/commentaries/print.antlers.php
+++ sources/webapp/resources/views/commentaries/print.antlers.php
@@ -1,4 +1,6 @@
 <header class="header">
+    <span class="running-title">{{ title }}</span>
+    <span class="running-authors">{{ assigned_authors | pluck('name') | join(' / ') }}</span>
     <img src="{{ config:app:url }}/img/ok-logo-text_{{ locale }}.svg" class="header-logo">
     {{ if original_language:handle && original_language:handle !== locale }}
         <div class="header-translation">
@@ -16,28 +18,46 @@
         {{ trans:edited_by }} {{ assigned_editors | pluck('name') | join(' / ') }}
     </p>
 </header>
-<section class="citation">
-    <p class="citation-label">
-        {{ trans:suggested_citation }}
-    </p>
-    <p class="citation-text">
-        {{ suggested_citation_long }}
-    </p>
-    <p class="citation-text">
-        {{ trans:short_citation }}: {{ suggested_citation_short }}
-    </p>
+
+<section class="status-of-processing">
+    <p>{{ trans:status_of_processing }} {{ last_modified format="d.m.Y" }}</p>
+    <p>{{ trans:generated_on }} {{ generation_date }}</p>
+    {{ if doi }}
+        <p>DOI: {{ doi }}</p>
+    {{ /if }}
 </section>
-<section class="legal-text">
-    {{ legal_text }}
-</section>
-<section class="toc">
-    <p class="toc-label">
+
+{{ if suggested_citation_long || suggested_citation_short }}
+    <section class="citation">
+        {{ if suggested_citation_long }}
+            <p class="citation-label">
+                {{ trans:suggested_citation }}
+            </p>
+            <p class="citation-text">
+                {{ suggested_citation_long }}
+            </p>
+        {{ /if }}
+        {{ if suggested_citation_short }}
+            <p class="citation-text">
+                {{ trans:short_citation }}: {{ suggested_citation_short }}
+            </p>
+        {{ /if }}
+    </section>
+{{ /if }}
+
+{{ if legal_text }}
+    <section class="legal-text">
+        {{ legal_text }}
+    </section>
+{{ /if }}
+
+<section class="entry-toc">
+    <p class="header-label">
         {{ trans:table_of_contents }}
     </p>
-    <div class="toc-list">
-        {{ toc }}
-    </div>
+    {{ toc }}
 </section>
+
 <main class="content">
     {{ content }}
 </main>
